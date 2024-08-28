6b1803989c531031a34e6905995e3c0f75066a53
Kas Buunk <kasbuunk@icloud.com>
2024-08-28 21:35:33 +0200

    feat: implement double fn

--- src/Chapter1.hs
+++ src/Chapter1.hs
@@ -1,4 +1,7 @@
-module Chapter1 (add_one) where
+module Chapter1 (add_one, double) where
 
 add_one :: Int -> Int
 add_one n = n + 1
+
+double :: Int -> Int
+double n = 2 * n
--- test/Chapter1Spec.hs
+++ test/Chapter1Spec.hs
@@ -1,10 +1,13 @@
 module Chapter1Spec (spec) where
 
 import Test.Hspec
-import Chapter1 (add_one)
+import Chapter1 (add_one, double)
 
 spec :: Spec
 spec = do
     describe "Chapter 1 Tests" $ do
         it "add one" $ do
             add_one 0 `shouldBe` 1
+
+        it "double" $ do
+            double 2 `shouldBe` 4
