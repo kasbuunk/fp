b35c6aee75a9e39a879c27261a9361705a917efa
Kas Buunk <kasbuunk@icloud.com>
2024-08-31 11:22:52 +0200

    chore: do exercise 1.1

--- src/Chapter1.hs
+++ src/Chapter1.hs
@@ -2,3 +2,6 @@ module Chapter1  where
 
 add_one :: Int -> Int
 add_one n = n + 1
+
+square :: Num a => a -> a
+square side = side * side
--- test/Chapter1Spec.hs
+++ test/Chapter1Spec.hs
@@ -8,3 +8,6 @@ spec = do
     describe "Chapter1" $ do
         it "import test" $ do
             add_one 0 `shouldBe` 1
+
+        it "Exercise 1.1: surface square" $ do
+            square 5 `shouldBe` 25
