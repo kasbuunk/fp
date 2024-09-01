c706efe346ccde751870291878906dfdfb103d8d
Kas Buunk <kasbuunk@icloud.com>
2024-09-01 08:44:52 +0200

    test: try Eq type class

--- test/Chapter3Spec.hs
+++ test/Chapter3Spec.hs
@@ -11,3 +11,18 @@ spec = do
 
         it "generate list from zero to n" $ do
             zeroto 8 `shouldBe` [0,1,2,3,4,5,6,7,8]
+
+        it "try Eq on Bool" $ do
+            False == False `shouldBe` True
+
+        it "try Eq on Char" $ do
+            'a' == 'b' `shouldBe` False
+
+        it "try Eq on String" $ do
+            "abc" == "abc" `shouldBe` True
+
+        it "try Eq on tuple" $ do
+            not (('a', False) == ('a', False)) `shouldBe` False
+
+        it "try Eq on list" $ do
+            [1,2] == [1,2,3] `shouldBe` False
