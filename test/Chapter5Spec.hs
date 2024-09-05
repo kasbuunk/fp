0d00abfa9d827777053b6e21e0c8209018a4ae41
Kas Buunk <kasbuunk@icloud.com>
2024-09-05 22:26:38 +0200

    feat: second generator depends on first

--- test/Chapter5Spec.hs
+++ test/Chapter5Spec.hs
@@ -11,3 +11,6 @@ spec = do
 
         it "cartesian product" $ do
             cartesian [1,2,3] [4,5] `shouldBe` [(1,4),(1,5),(2,4),(2,5),(3,4),(3,5)]
+
+        it "second generator depends on first" $ do
+            [(x,y) | x <- [1..3], y <- [x..3]] `shouldBe` [(1,1),(1,2),(1,3),(2,2),(2,3),(3,3)]
