7a78d52f809aaedfb58fcc41bd2c2c38bcaa6bb6
Kas Buunk <kasbuunk@icloud.com>
2024-09-05 21:23:28 +0200

    feat: swap tuple elements with lambda

--- test/Chapter4Spec.hs
+++ test/Chapter4Spec.hs
@@ -287,3 +287,6 @@ spec = do
 
         it "give itself and its square" $ do
             map (\x -> (x, x*x)) [0,1,2,3] `shouldBe` [(0,0),(1,1),(2,4),(3,9)]
+
+        it "swap tuple elements" $ do
+            map (\(x,y) -> (y,x)) [(0,'a'),(1,'b')] `shouldBe` [('a',0),('b',1)]
