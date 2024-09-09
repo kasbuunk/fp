65c6d4162697031f9cc220460d37dba6b9a29ffe
Kas Buunk <kasbuunk@icloud.com>
2024-09-09 19:35:40 +0200

    feat: define recursive multiplication

--- src/Chapter6.hs
+++ src/Chapter6.hs
@@ -2,4 +2,8 @@ module Chapter6 where
 
 fac :: Int -> Int
 fac 0 = 1
-fac n = n * fac (n-1)
+fac n = n * fac (n - 1)
+
+recursiveMultiply :: Int -> Int -> Int
+recursiveMultiply _ 0 = 0
+recursiveMultiply x y = x + recursiveMultiply x (y - 1)
--- test/Chapter6Spec.hs
+++ test/Chapter6Spec.hs
@@ -11,3 +11,10 @@ spec = do
       fac 1 `shouldBe` 1
       fac 2 `shouldBe` 2
       fac 3 `shouldBe` 6
+
+    it "recursive multiplication" $ do
+      recursiveMultiply 0 0 `shouldBe` 0
+      recursiveMultiply 1 0 `shouldBe` 0
+      recursiveMultiply 0 1 `shouldBe` 0
+      recursiveMultiply 1 1 `shouldBe` 1
+      recursiveMultiply 6 5 `shouldBe` 30
