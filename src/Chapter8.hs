9cbecb649b1690b4f252298c8877969921f1236b
Kas Buunk <kasbuunk@icloud.com>
2024-09-24 21:33:42 +0200

    feat: data declaration with moves

--- src/Chapter8.hs
+++ src/Chapter8.hs
@@ -4,3 +4,17 @@ type Assoc k v = [(k, v)]
 
 find :: (Eq k) => k -> Assoc k v -> v
 find key table = head [value | (x, value) <- table, key == x]
+
+data Move = North | South | East | West
+
+type Pos = (Double, Double)
+
+move :: Move -> Pos -> Pos
+move North (x, y) = (x, y + 1)
+move South (x, y) = (x, y - 1)
+move West (x, y) = (x - 1, y)
+move East (x, y) = (x + 1, y)
+
+moves :: [Move] -> Pos -> Pos
+moves [] pos = pos
+moves (m : ms) pos = moves ms (move m pos)
--- test/Chapter8Spec.hs
+++ test/Chapter8Spec.hs
@@ -9,3 +9,8 @@ spec = do
     it "dictionary" $ do
       let t = [("foo", 0), ("bar", 1), ("key", 7), ("baz", 10)]
        in find "key" t `shouldBe` 7
+
+    it "move positions" $ do
+      moves [North, North] (0, 0) `shouldBe` (0, 2)
+      moves [West, South] (0, 0) `shouldBe` (-1, -1)
+      moves [North, North, West, South, East, West, North] (0, 0) `shouldBe` (-1, 2)
