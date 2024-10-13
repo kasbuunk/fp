113d3851f893f1daa6c1aca821262906e166ff0e
Kas Buunk <kasbuunk@icloud.com>
2024-10-13 15:53:26 +0200

    chore: redefine choices with list comprehension

--- src/Chapter9.hs
+++ src/Chapter9.hs
@@ -58,6 +58,9 @@ perms (x : xs) = concat (map (interleave x) (perms xs))
 choices :: [a] -> [[a]]
 choices = concat . map perms . subs
 
+choices' :: [a] -> [[a]]
+choices' xs = [zs | ys <- subs xs, zs <- perms ys]
+
 solution :: Expr -> [Int] -> Int -> Bool
 solution e ns n =
   elem (values e) (choices ns) && eval e == [n]
