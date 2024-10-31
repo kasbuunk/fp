b2174366253c302a5db3f8d5147430c8f7c6148b
Kas Buunk <kasbuunk@icloud.com>
2024-10-31 19:56:03 +0100

    feat: subtract natural numbers

--- src/Chapter16.hs
+++ src/Chapter16.hs
@@ -4,3 +4,10 @@ power :: Int -> Int -> Int
 power x n
   | n == 0 = 1
   | otherwise = x * power x (n - 1)
+
+data Nat = Zero | Succ Nat deriving (Show)
+
+sub :: Nat -> Nat -> Nat
+sub Zero m = Zero
+sub (Succ n) Zero = Succ n
+sub (Succ n) (Succ m) = sub n m
--- test/Chapter16Spec.hs
+++ test/Chapter16Spec.hs
@@ -9,3 +9,7 @@ spec = do
     it "power without overlap" $ do
       power 1 0 `shouldBe` 1
       power 2 5 `shouldBe` 32
+
+    it "subtract" $ do
+      show (sub (Succ (Succ Zero)) (Succ Zero)) `shouldBe` "Succ Zero"
+      show (sub (Succ Zero) (Succ (Succ Zero))) `shouldBe` "Zero"
